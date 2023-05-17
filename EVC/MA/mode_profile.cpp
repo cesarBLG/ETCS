ad99e5ce0331919d34c83583f79cc2397b1d2a5e
César Benito Lamata <cesarbema2009@hotmail.com>
2023-05-17 09:20:28 +0200

    Switch to MPL license

--- EVC/MA/mode_profile.cpp
+++ EVC/MA/mode_profile.cpp
@@ -1,3 +1,11 @@
+/*
+ * European Train Control System
+ * Copyright (C) 2019-2023  César Benito <cesarbema2009@hotmail.com>
+ * 
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
 #include "mode_profile.h"
 #include "../Procedures/mode_transition.h"
 std::list<mode_profile> mode_profiles;
