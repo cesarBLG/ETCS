8a8c0d2d68278614db1e6706111522053c846e0d
milek7 <me@milek7.pl>
2023-06-27 15:32:53 +0200

    wasm api changes

--- platform/simrail_platform.cpp
+++ platform/simrail_platform.cpp
@@ -7,44 +7,28 @@
 #include "simrail_platform.h"
 #include "platform_runtime.h"
 
+using namespace PlatformUtil;
+
 #define EXPORT_FUNC extern "C" __attribute__((visibility("default")))
 #define IMPORT_FUNC(m, n) extern "C" __attribute__ ((import_module(m))) __attribute__ ((import_name(n)))
 
-EXPORT_FUNC void* alloc_mem(int size) {
-    return malloc(size);
+namespace api {
+	IMPORT_FUNC("simrail_base_v1", "get_timer") int64_t get_timer();
+	IMPORT_FUNC("simrail_base_v1", "get_timestamp") int64_t get_timestamp();
+	IMPORT_FUNC("simrail_base_v1", "read_file") char* read_file(const char* t, size_t tlen, size_t* len);
+	IMPORT_FUNC("simrail_base_v1", "write_file") uint32_t write_file(const char* t, size_t tlen, const char* c, size_t len);
+	IMPORT_FUNC("simrail_base_v1", "debug_print") void debug_print(const char* t, size_t len);
+	IMPORT_FUNC("simrail_base_v1", "delay") void delay(int32_t ms, void*, void*, void*);
+	IMPORT_FUNC("simrail_base_v1", "on_quit_request") void on_quit_request(void*, void*, void*);
+	IMPORT_FUNC("simrail_base_v1", "on_quit") void on_quit(void*, void*, void*);
+	IMPORT_FUNC("simrail_base_v1", "quit") void quit();
+	IMPORT_FUNC("simrail_base_v1", "open_socket") uint32_t open_socket(const char* name, size_t nlen, uint32_t tid);
 }
-
-EXPORT_FUNC void free_mem(void *mem) {
-    free(mem);
-}
-
-using namespace PlatformUtil;
-
-void callback_fulfill_void(Fulfiller<void>* fulfiller) {
-	fulfiller->fulfill(false);
-	while (DeferredFulfillment::execute());
-	delete fulfiller;
-}
-
-void callback_cancel_void(Fulfiller<void>* fulfiller) {
-	delete fulfiller;
-}
-
-IMPORT_FUNC("simrail", "get_timer") int64_t get_timer();
-IMPORT_FUNC("simrail", "get_timestamp") int64_t get_timestamp();
-IMPORT_FUNC("simrail", "read_file") char* read_file(const char* t, size_t tlen, size_t* len);
-IMPORT_FUNC("simrail", "write_file") uint32_t write_file(const char* t, size_t tlen, const char* c, size_t len);
-IMPORT_FUNC("simrail", "debug_print") void debug_print(const char* t, size_t len);
-IMPORT_FUNC("simrail", "delay") void delay(int32_t ms, void*, void*, void*);
-IMPORT_FUNC("simrail", "on_quit_request") void on_quit_request(void*, void*, void*);
-IMPORT_FUNC("simrail", "on_quit") void on_quit(void*, void*, void*);
-IMPORT_FUNC("simrail", "quit") void quit();
-IMPORT_FUNC("simrail", "open_socket") uint32_t open_socket(const char* name, size_t nlen, uint32_t tid);
 
 extern "C" ssize_t writev(int fd, iovec *iov, int iovcnt) {
 	ssize_t total = 0;
 	for (int i = 0; i < iovcnt; i++) {
-		::debug_print((char*)iov[i].iov_base, iov[i].iov_len);
+		api::debug_print((char*)iov[i].iov_base, iov[i].iov_len);
 		total += iov[i].iov_len;
 	}
     return total;
@@ -56,6 +40,14 @@ extern "C" int close(int fd) {
     return 0;
 }
 
+EXPORT_FUNC void* alloc_mem(int size) {
+    return malloc(size);
+}
+
+EXPORT_FUNC void free_mem(void *mem) {
+    free(mem);
+}
+
 EXPORT_FUNC void init() {
 #ifdef EVC
 	platform = std::make_unique<SimrailBasePlatform>();
@@ -65,16 +57,26 @@ EXPORT_FUNC void init() {
 	on_platform_ready();
 }
 
+static void callback_fulfill_void(Fulfiller<void>* fulfiller) {
+	fulfiller->fulfill(false);
+	while (DeferredFulfillment::execute());
+	delete fulfiller;
+}
+
+static void callback_cancel_void(Fulfiller<void>* fulfiller) {
+	delete fulfiller;
+}
+
 SimrailBasePlatform::SimrailBasePlatform() {
 	PlatformUtil::DeferredFulfillment::list = &event_list;
 }
 
 int64_t SimrailBasePlatform::get_timer() {
-	return ::get_timer();
+	return api::get_timer();
 }
 
 int64_t SimrailBasePlatform::get_timestamp() {
-	return ::get_timestamp();
+	return api::get_timestamp();
 }
 
 BasePlatform::DateTime SimrailBasePlatform::get_local_time() {
@@ -83,58 +85,60 @@ BasePlatform::DateTime SimrailBasePlatform::get_local_time() {
 
 std::optional<std::string> SimrailBasePlatform::read_file(const std::string_view path) {
 	size_t len;
-	char *ret = ::read_file(path.data(), path.size(), &len);
+	char *ret = api::read_file(path.data(), path.size(), &len);
 	if (!ret)
 		return std::nullopt;
 	std::string str(ret, len);
-	::free_mem(ret);
+	free_mem(ret);
 	return str;
 }
 
 bool SimrailBasePlatform::write_file(const std::string_view path, const std::string_view contents) {
-	return ::write_file(path.data(), path.size(), contents.data(), contents.size()) == 1;
+	return api::write_file(path.data(), path.size(), contents.data(), contents.size()) == 1;
 }
 
 void SimrailBasePlatform::debug_print(const std::string_view msg) {
-	::debug_print(msg.data(), msg.size());
+	api::debug_print(msg.data(), msg.size());
 }
 
 Promise<void> SimrailBasePlatform::delay(int ms) {
 	auto pair = PromiseFactory::create<void>();
-	::delay(ms, (void*)&callback_fulfill_void, (void*)&callback_cancel_void, new Fulfiller(std::move(pair.second)));
+	api::delay(ms, (void*)&callback_fulfill_void, (void*)&callback_cancel_void, new Fulfiller(std::move(pair.second)));
 	return std::move(pair.first);
 }
 
 Promise<void> SimrailBasePlatform::on_quit_request() {
 	auto pair = PromiseFactory::create<void>();
-	::on_quit_request((void*)&callback_fulfill_void, (void*)&callback_cancel_void, new Fulfiller(std::move(pair.second)));
+	api::on_quit_request((void*)&callback_fulfill_void, (void*)&callback_cancel_void, new Fulfiller(std::move(pair.second)));
 	return std::move(pair.first);
 }
 
 Promise<void> SimrailBasePlatform::on_quit() {
 	auto pair = PromiseFactory::create<void>();
-	::on_quit((void*)&callback_fulfill_void, (void*)&callback_cancel_void, new Fulfiller(std::move(pair.second)));
+	api::on_quit((void*)&callback_fulfill_void, (void*)&callback_cancel_void, new Fulfiller(std::move(pair.second)));
 	return std::move(pair.first);
 }
 
 std::unique_ptr<BasePlatform::BusSocket> SimrailBasePlatform::open_socket(const std::string_view channel, uint32_t tid) {
-	uint32_t handle = ::open_socket(channel.data(), channel.size(), tid);
+	uint32_t handle = api::open_socket(channel.data(), channel.size(), tid);
 	if (!handle)
 		return nullptr;
 	return std::make_unique<SimrailBusSocket>(handle);
 }
 
 void SimrailBasePlatform::quit() {
-	::quit();
+	api::quit();
 }
 
-IMPORT_FUNC("simrail", "socket_close") void socket_close(uint32_t handle);
-IMPORT_FUNC("simrail", "socket_broadcast") void socket_broadcast(uint32_t handle, const char* msg, size_t len);
-IMPORT_FUNC("simrail", "socket_broadcast_tid") void socket_broadcast_tid(uint32_t handle, uint32_t tid, const char* msg, size_t len);
-IMPORT_FUNC("simrail", "socket_send_to") void socket_send_to(uint32_t handle, uint32_t uid, const char* msg, size_t len);
-IMPORT_FUNC("simrail", "socket_receive") void socket_receive(uint32_t handle, void*, void*, void*);
+namespace api {
+	IMPORT_FUNC("simrail_base_v1", "socket_close") void socket_close(uint32_t handle);
+	IMPORT_FUNC("simrail_base_v1", "socket_broadcast") void socket_broadcast(uint32_t handle, const char* msg, size_t len);
+	IMPORT_FUNC("simrail_base_v1", "socket_broadcast_tid") void socket_broadcast_tid(uint32_t handle, uint32_t tid, const char* msg, size_t len);
+	IMPORT_FUNC("simrail_base_v1", "socket_send_to") void socket_send_to(uint32_t handle, uint32_t uid, const char* msg, size_t len);
+	IMPORT_FUNC("simrail_base_v1", "socket_receive") void socket_receive(uint32_t handle, void*, void*, void*);
+}
 
-void callback_fulfill_socket_receive(Fulfiller<BasePlatform::BusSocket::ReceiveResult>* fulfiller, uint32_t action, uint32_t tid, uint32_t uid, char* data, size_t len) {
+static void callback_fulfill_socket_receive(Fulfiller<BasePlatform::BusSocket::ReceiveResult>* fulfiller, uint32_t action, uint32_t tid, uint32_t uid, char* data, size_t len) {
 	BasePlatform::BusSocket::PeerId peer { tid, uid };
 	if (action == 1) {
 		fulfiller->fulfill(BasePlatform::BusSocket::JoinNotification { peer }, false);
@@ -142,13 +146,13 @@ void callback_fulfill_socket_receive(Fulfiller<BasePlatform::BusSocket::ReceiveR
 		fulfiller->fulfill(BasePlatform::BusSocket::LeaveNotification { peer }, false);
 	} else if (action == 3) {
 		fulfiller->fulfill(BasePlatform::BusSocket::Message { peer, std::string(data, len) }, false);
-		::free_mem(data);
+		free_mem(data);
 	}
 	while (DeferredFulfillment::execute());
 	delete fulfiller;
 }
 
-void callback_cancel_socket_receive(Fulfiller<BasePlatform::BusSocket::ReceiveResult>* fulfiller) {
+static void callback_cancel_socket_receive(Fulfiller<BasePlatform::BusSocket::ReceiveResult>* fulfiller) {
 	delete fulfiller;
 }
 
@@ -156,37 +160,26 @@ SimrailBasePlatform::SimrailBusSocket::SimrailBusSocket(uint32_t handle) : handl
 }
 
 SimrailBasePlatform::SimrailBusSocket::~SimrailBusSocket() {
-	::socket_close(handle);
+	api::socket_close(handle);
 }
 
 void SimrailBasePlatform::SimrailBusSocket::broadcast(const std::string_view data) {
-	::socket_broadcast(handle, data.data(), data.size());
+	api::socket_broadcast(handle, data.data(), data.size());
 }
 
 void SimrailBasePlatform::SimrailBusSocket::broadcast(uint32_t tid, const std::string_view data) {
-	::socket_broadcast_tid(handle, tid, data.data(), data.size());
+	api::socket_broadcast_tid(handle, tid, data.data(), data.size());
 }
 
 void SimrailBasePlatform::SimrailBusSocket::send_to(uint32_t uid, const std::string_view data) {
-	::socket_send_to(handle, uid, data.data(), data.size());
+	api::socket_send_to(handle, uid, data.data(), data.size());
 }
 
 PlatformUtil::Promise<BasePlatform::BusSocket::ReceiveResult> SimrailBasePlatform::SimrailBusSocket::receive() {
 	auto pair = PromiseFactory::create<BasePlatform::BusSocket::ReceiveResult>();
-	::socket_receive(handle, (void*)&callback_fulfill_socket_receive, (void*)&callback_cancel_socket_receive, new Fulfiller(std::move(pair.second)));
+	api::socket_receive(handle, (void*)&callback_fulfill_socket_receive, (void*)&callback_cancel_socket_receive, new Fulfiller(std::move(pair.second)));
 	return std::move(pair.first);
 }
-
-//IMPORT_FUNC("simrail", "on_input_event") void on_input_event(void *ticket);
-
-/*
-EXPORT_FUNC void callback_inputevent(Fulfiller<UiPlatform::InputEvent>* fulfiller, UiPlatform::InputEvent::Action action, float x, float y)
-{
-	fulfiller->fulfill(UiPlatform::InputEvent{action, x, y}, false);
-	delete fulfiller;
-	while (DeferredFulfillment::execute());
-}
-*/
 
 void SimrailUiPlatform::set_color(Color c) {
 }
@@ -253,9 +246,4 @@ void SimrailUiPlatform::set_brightness(int vol) {
 
 Promise<UiPlatform::InputEvent> SimrailUiPlatform::on_input_event() {
 	return {};
-	/*
-	auto pair = PromiseFactory::create<InputEvent>();
-	::on_input_event(new Fulfiller<InputEvent>(std::move(pair.second)));
-	return std::move(pair.first);
-	*/
 }
