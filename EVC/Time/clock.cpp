fb6536c6821c7d1e168d71e644a7722da9e3c9a6
me <me@milek7.pl>
2023-07-17 22:54:24 +0200

    fix negative timeoffset

--- EVC/Time/clock.cpp
+++ EVC/Time/clock.cpp
@@ -15,16 +15,12 @@ int64_t get_milliseconds()
 }
 BasePlatform::DateTime offset_time() {
     BasePlatform::DateTime clock = platform->get_local_time();
-    clock.second += TimeOffset::offset;
-    while (clock.second >= 60) {
-        clock.second -= 60;
-        clock.minute++;
-    }
-    while (clock.minute >= 60) {
-        clock.minute -= 60;
-        clock.hour++;
-    }
-    while (clock.hour >= 24)
-        clock.hour -= 24;
+    int secs = clock.hour * 3600 + clock.minute * 60 + clock.second + TimeOffset::offset;
+    while (secs < 0)
+        secs += 86400;
+    secs %= 86400;
+    clock.hour = secs / 3600;
+    clock.minute = (secs - clock.hour * 3600) / 60;
+    clock.second = secs - clock.hour * 3600 - clock.minute * 60;
     return clock;
 }
