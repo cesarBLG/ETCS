c73975a6c533e2d19b0fa5f8383fb133d81cc6d2
César Benito Lamata <cesarbema2009@hotmail.com>
2023-08-21 12:33:51 +0200

    Fix cold movement file creation

--- EVC/TrainSubsystems/cold_movement.cpp
+++ EVC/TrainSubsystems/cold_movement.cpp
@@ -44,9 +44,9 @@ void initialize_cold_movement()
 void save_cold_data(std::string field, json &value)
 {
     auto contents = platform->read_file("cold_data.json");
-    if (!contents)
-        return;
-    json j = json::parse(*contents);
+    json j;
+    if (contents)
+        j = json::parse(*contents);
     j[field] = value;
     platform->write_file("cold_data.json", j.dump());
 }
