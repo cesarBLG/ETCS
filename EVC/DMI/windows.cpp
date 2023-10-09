6fe6f7c17627d8420a0487bbfe81df31b405cdcf
Bartłomiej Skwara <bartek@skwara.me>
2023-10-09 12:50:46 +0200

    Chineese language support

--- EVC/DMI/windows.cpp
+++ EVC/DMI/windows.cpp
@@ -249,7 +249,7 @@ json sr_data_window()
 json language_window()
 {
     json j = R"({"active":"language_window"})"_json;
-    j["WindowDefinition"] = build_input_window(get_text("Language"), {build_input_field("", language, {"en","es","fr","de","pt","it","nl","sv","el","hu","pl"})});
+    j["WindowDefinition"] = build_input_window(get_text("Language"), {build_input_field("", language, {"en","es","fr","de","pt","it","nl","sv","el","hu","pl","zh_Hans","zh_Hant"})});
     return j;
 }
 json set_vbc_window()
