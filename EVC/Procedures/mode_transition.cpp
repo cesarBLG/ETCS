762b7a972d9f1a745407e11015faad10e628898b
César Benito Lamata <cesarbema2009@hotmail.com>
2024-03-01 08:28:13 +0100

    Report transitions to SB mode with msg 136

--- EVC/Procedures/mode_transition.cpp
+++ EVC/Procedures/mode_transition.cpp
@@ -379,7 +379,7 @@ void update_mode_status()
         delete_information(prevmode);
         if (mode != Mode::NP && mode != Mode::PS && (mode != Mode::SH || prevmode != Mode::PS)) {
             if ((level == Level::N2 || level == Level::N3) && !end_mission
-            && mode != Mode::SB // Out of SRS condition: do not start session to report mode change to SB
+            //&& mode != Mode::SB // Out of SRS condition: do not start session to report mode change to SB
             && (!supervising_rbc || supervising_rbc->status == session_status::Inactive) && rbc_contact_valid) {
                 set_supervising_rbc(*rbc_contact);
                     if (supervising_rbc)
