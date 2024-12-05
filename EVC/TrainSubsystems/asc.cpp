e54de358d6fa3e9203a155a954c599162f112914
César Benito Lamata <cesarbema2009@hotmail.com>
2024-12-05 13:06:54 +0100

    Implement ASC via json interface

--- EVC/TrainSubsystems/asc.cpp
+++ EVC/TrainSubsystems/asc.cpp
@@ -1,12 +1,15 @@
 #include "../Supervision/supervision.h"
 #include "../Supervision/supervision_targets.h"
 #include "asc.h"
+#include <nlohmann/json.hpp>
+using json = nlohmann::json;
 // Interface with automatic speed control
 std::string asc_status;
 bool asc_fitted=true;
 void update_asc()
 {
     if (mode == Mode::FS && asc_fitted) {
+        json status;
         std::vector<std::pair<double, double>> targets;
         const std::list<std::shared_ptr<target>> &supervised_targets = get_supervised_targets();
         for (auto &t : supervised_targets) {
@@ -20,10 +23,17 @@ void update_asc()
             double d = pos - (t->is_EBD_based ? d_maxsafefront(t->get_target_position()) : d_estfront);
             targets.push_back({d, t->get_target_speed()});
         }
-        asc_status = std::to_string(V_perm);
+        status["AllowedSpeedMpS"] = V_perm;
+        status["ReleaseSpeedMpS"] = V_release;
+        json targs;
         for (auto kvp : targets) {
-            asc_status += ";"+std::to_string(round(kvp.first + current_odometer_orientation * odometer_value))+";"+std::to_string(kvp.second);
+            json j;
+            j["TargetLocationM"] = round(kvp.first + current_odometer_orientation * odometer_value);
+            j["TargetSpeedMpS"] = kvp.second;
+            targs.push_back(j);
         }
+        status["Targets"] = targs;
+        asc_status = status.dump();
     } else {
         asc_status = "";
     }
